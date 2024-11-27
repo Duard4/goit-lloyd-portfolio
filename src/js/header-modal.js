2a57028faffb0c2cc093b15688f7f837324e8e58
Anton2 Spaine <avetrov392@gmail.com>
2024-11-27 01:49:28 -0800

    modal

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -13,7 +13,19 @@ if (openModalBtn && closeModalButton && menuModal) {
   });
 
   menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+
+      const targetId = link.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+
       menuModal.classList.remove('open');
     });
   });
